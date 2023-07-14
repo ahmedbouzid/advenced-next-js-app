db1a71dd944b0a59a1cf0fc510c0ddeca4dcdf7d
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-14 20:27:03 +0200

    complete auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
  <>
  <div className='p-4'>
- <Button size={'sm'} variant={'destructive'} >Click Me</Button>
+ <UserButton  afterSignOutUrl="/"/>
 
  </div>
  </>
