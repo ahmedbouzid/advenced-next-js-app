0d7ce1a690592faed3a223688b000ea801637995
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-15 10:25:33 +0200

    prepare form and install thier dependecies

--- components/modals/StoreModal.tsx
+++ components/modals/StoreModal.tsx
@@ -1,21 +1,72 @@
 "use client"
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"
+
 import { useStoreModal } from "@/hooks/useStoreModal"
-import { Modal } from "@/components/ui/modal"
+import { Modal } from "@/components/ui/modal" 
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 
-
+const formSchema = z.object({
+    name: z.string().min(1)
+}) ;
 export const StoreModal = () => {
 
     const storeModal = useStoreModal()
 
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver : zodResolver(formSchema),
+        defaultValues : {
+            name : "" ,
+        }   
+    })
+    const onSubmit = async (values :z.infer<typeof formSchema>) => {
+        /// Create Store
+        console.log(values)
+    }
     return (
         <Modal
         title="Create Store"
         description="Add a new Store"
         isOpen={storeModal.isOpen}
         onClose={storeModal.onClose}>
-            Create store form
+                <div>
+                    <div className="space-y-4 py-2 pb-4">
+                        <Form {...form}>
+
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                                <FormField
+                                control={form.control}
+                                render={({field})=> (
+                                    <FormItem>
+                                        <FormLabel >
+                                            Name
+                                        </FormLabel>
+                                        <FormControl> 
+                                            <Input placeholder="Store " {...field} />
+                                        </FormControl>
+                                        <FormMessage >
+                                        </FormMessage>
+                                    </FormItem>
+                                )}
+                                name="name" />
+
+                                <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                    <Button variant={"outline"} 
+                                    onClick={storeModal.onClose} >
+                                        Cancel
+                                         </Button>
+                                    <Button type="submit" > Add </Button>
+                                </div>
+
+                            </form>
+                        </Form>
+                    </div>
+                </div>
         </Modal>
     )
 }
